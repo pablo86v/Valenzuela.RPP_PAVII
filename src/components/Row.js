a6d7a7b9d34d06d18c2606d55295747f2f350a58
Pablo Valenzuela <pablo86v@gmail.com>
2021-11-16 18:51:48 -0300

    + componentes

---
+++ src/components/Row.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Row = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Row.propTypes = {
+    
+};
+
+export default Row;
---
+++ src/components/Select.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Select = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Select.propTypes = {
+    
+};
+
+export default Select;
